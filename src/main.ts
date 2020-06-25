5c25f9be94c50def3344be53fd69cf8c7923eb41
Krisztian Kugler <krisztian.kugler@edreamsodigeo.com>
2020-06-25 08:44:36 +0200

    first set completed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import "./styles.scss";
 import { Board } from "./board";
-import { oldestGame, oldestGame2, oldestGame3, oldestGame4, oldestGame5, oldestGame6, agatka } from "./set1";
+import { oldestGame, oldestGame2, oldestGame3, oldestGame4, oldestGame5, oldestGame6, agatka, doggie } from "./set1";
 
-const board = new Board(agatka);
+const board = new Board(doggie);
 
 board.mount(".board-slot");
 console.log(board);
